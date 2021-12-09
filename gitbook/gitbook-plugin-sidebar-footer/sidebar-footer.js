d5fcc15f916cd635e79e53dc43d9130360881e9e
JustPilz <dkrivyakov@gmail.com>
2021-12-10 01:03:36 +0300

    some fixes

--- gitbook/gitbook-plugin-sidebar-footer/sidebar-footer.js
+++ gitbook/gitbook-plugin-sidebar-footer/sidebar-footer.js
@@ -1,61 +1,65 @@
 require(['gitbook', 'jQuery'], function (gitbook, $) {
-
   var sidebarFooter;
   var pluginConfig = {};
-
 
   /**
    * function to get json from url with callback
    */
-  var getJSON = function(url, callback) {
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', url, true);
-      xhr.responseType = 'json';
-      xhr.onload = function() {
-        var status = xhr.status;
-        if (status == 200) {
-          callback(null, xhr.response);
-        } else {
-          callback(status);
-        }
-      };
-      xhr.send();
+  var getJSON = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function () {
+      var status = xhr.status;
+      if (status == 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status);
+      }
+    };
+    xhr.send();
   };
 
-
-function getOnline() {
-
-    getJSON('https://mafia2-online.com/api/v1/server',
-    function(err, data) {
-        var online = null;
-        if (err != null) {
-            return online = "N/A";
-        } else {
-            // see every area, create option element, append it to select
-            data.forEach(function(server) {
-                if(server.ip == "139.59.142.46") {
-                  return online = server.lastPlayersCount;
-                }
-            });
-        }
-        document.getElementById("online").innerHTML = online;
+  function getOnline() {
+    getJSON('https://mafia2-online.com/api/v1/server', function (err, data) {
+      var online = null;
+      if (err != null) {
+        return (online = 'N/A');
+      } else {
+        // see every area, create option element, append it to select
+        data.forEach(function (server) {
+          if (server.ip == '139.59.142.46') {
+            return (online = server.lastPlayersCount);
+          }
+        });
+      }
+      document.getElementById('online').innerHTML = online;
     });
-}
+  }
 
+  function getCurrentDate() {
+    const t = new Date();
+    const date = ('0' + t.getDate()).slice(-2);
+    const month = ('0' + (t.getMonth() + 1)).slice(-2);
+    const year = t.getFullYear();
+    return `${date}.${month}.${year}`;
+  }
 
   function initializePlugin(config) {
     pluginConfig = config['sidebar-footer'];
 
     var text = pluginConfig.text;
 
-    sidebarFooter
-      = '<div class="sidebar-footer">'
-        //+ '<div class="line">Игроков онлайн: <span id="online" class="online"></span></div>'
-        + '<div class="line">Сервер: <span id="servername">NCRP</span></div>'
-        + '<div class="line">IP: <span id="serverip">139.59.142.46</span> | Порт: <span id="serverport">7777</span></div>'
-       // + '<p>'+text+'<p>'
-      + '</div>'
-      ;
+    var dateNow = getCurrentDate();
+
+    sidebarFooter =
+      '<div class="sidebar-footer">' +
+      //+ '<div class="line">Игроков онлайн: <span id="online" class="online"></span></div>'
+      `<div class="line">Статус на ${dateNow}</div>` +
+      '<div class="line">СЕРВЕР РАБОТАЕТ</div>' +
+      //'<div class="line">IP: <span id="serverip">139.59.142.46</span> | Порт: <span id="serverport">7777</span></div>' +
+      //'<p>' + dateNow + '<p>'
+      '</div>';
 
     /*
       = '<a id="forkmegithub" href="' + pluginConfig.url + '">'
@@ -74,7 +78,7 @@ function getOnline() {
     initializePlugin(config);
     getOnline();
     setInterval(getOnline, 60000);
-/*
+    /*
     gitbook.toolbar.createButton({
       icon: 'fa fa-github',
       label: 'GitHub',
@@ -86,7 +90,7 @@ function getOnline() {
     */
   });
 
-  gitbook.events.bind('page.change', function() {
+  gitbook.events.bind('page.change', function () {
     var summaryUl = $('.book .book-summary');
     summaryUl.append(sidebarFooter);
     getOnline();
